8140001fa7f587b45dab947308050ac5d1c9affd
Maks3w <Maks3w@users.noreply.github.com>
2018-04-25 09:06:48 +0200

    [ch6017] BSD-3 LICENSE

--- quaderno_sdk/__init__.py
+++ quaderno_sdk/__init__.py
@@ -1,18 +1,4 @@
 #!/usr/bin/env python
-#
-#           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#                    Version 2, December 2004
-#
-# Copyright 2015-2016 calvin
-#
-# Everyone is permitted to copy and distribute verbatim or modified
-# copies of this license document, and changing it is allowed as long
-# as the name is changed.
-#
-#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-#
-#  0. You just DO WHAT THE FUCK YOU WANT TO.
 
 
 __version__ = '0.0.3'
